1bf6b6ce9328413f3ed859a21c2042b60ed1bffb
Matheus Alves <malves224@gmail.com>
2022-03-01 16:51:38 -0300

    adiciona teste de integraçao para rota pages

--- tests/mock/service/pages.js
+++ tests/mock/service/pages.js
@@ -1,6 +1,7 @@
 const menuWithPages = require('../db/AllItemsWithPages.json');
 const acessPermission = require('../db/AcessPermission.json');
 const pages = require('../db/page.json');
+const actions = require('../db/actions.json');
 
 const getAllItemsMenuWithPagesFake = async () => {
   return Promise.resolve(menuWithPages)
@@ -33,8 +34,36 @@ const getPagesAllowedByPerfilFake = async (idPerfil) => {
   return Promise.resolve(pagesAllowed);
 }
 
+const sequelizeQueryFake = async (_query, options) => {
+  const { replacements } = options;
+  let pagesAllowed = [];
+  const permissionFromIdPerfil = acessPermission
+    .filter((perm) => perm["id_perfil"] === replacements.idPerfil);
+    permissionFromIdPerfil.forEach((permission) => {
+    const {id, name, route} = pages.find((page) => page.id === permission["id_page"]);
+    pagesAllowed = [...pagesAllowed, {id, name, route}];
+  });
+  return Promise.resolve(pagesAllowed);
+}
+
+const getAllPageWithActionsFake = async () => {
+  let pagesWithActions = [];
+  pages.forEach(({id, name, route}) => {
+    const pageForAdd = {
+      id,
+      name,
+      route,
+      actions: actions.filter((action) => action.idPage === id)
+    }
+    pagesWithActions = [...pagesWithActions, pageForAdd] 
+  });
+  return Promise.resolve(pagesWithActions);
+}
+
 module.exports = {
   getAllItemsMenuWithPagesFake,
   getAllPagesPermissionsByPerfilFake,
-  getPagesAllowedByPerfilFake
+  getPagesAllowedByPerfilFake,
+  sequelizeQueryFake,
+  getAllPageWithActionsFake
 }
